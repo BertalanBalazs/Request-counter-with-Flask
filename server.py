8320d66db89cffcc2c5cbada75074f85780ac61e
Bertalan Balázs <bertalan10@gmail.com>
2018-11-21 09:00:23 +0100

    simple finish

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
 from flask import Flask, redirect, render_template
 app = Flask(__name__)
 
-counts = 0
+counts = {
+    'GET': 0,
+    'POST': 0,
+    'DELETE': 0,
+    'PUT': 0
+}
 
 @app.route('/')
 def route_main():
-    return render_template('index.html' , counts = counts)
-
+    return render_template('index.html', counts=counts)
 
 
 @app.route('/request-counter')
 def route_request_counter():
-    counts += 1
-    return redirect('/', counts = counts)
+    global counts
+    if request
+    counts
+    return redirect('/')
 
 
 if __name__ == '__main__':
