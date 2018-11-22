55905e678c13c230ea115001e44c489ca9a14e40
Bertalan Balázs <bertalan10@gmail.com>
2018-11-22 22:06:20 +0100

    "succesful with every function and feature"

--- server.py
+++ server.py
@@ -1,24 +1,44 @@
-from flask import Flask, redirect, render_template
+from flask import Flask, redirect, render_template, request
+
+
 app = Flask(__name__)
 
-counts = {
-    'GET': 0,
-    'POST': 0,
-    'DELETE': 0,
-    'PUT': 0
-}
 
 @app.route('/')
 def route_main():
-    return render_template('index.html', counts=counts)
+    return render_template('index.html')
 
 
-@app.route('/request-counter')
+@app.route('/request-counter', methods=['GET', 'POST', 'DELETE', 'PUT'])
 def route_request_counter():
-    global counts
-    if request
-    counts
+    counts = import_data()
+    counts[request.method] += 1
+    export_data(counts)
     return redirect('/')
+
+
+@app.route('/statistics')
+def route_statistic():
+    counts = import_data()
+    return render_template('statistics.html',counts=counts)
+
+
+
+def export_data(dict):
+    with open('request_counts.txt', 'w') as data:
+        for key, value in dict.items():
+            data.write(f'{key}: {value}\n')
+
+
+
+def import_data():
+    d = {}
+    with open("request_counts.txt") as f:
+        for line in f:
+            key, val = line.strip().split(':', 1)
+            d[key] = int(val)
+    print(d)
+    return d
 
 
 if __name__ == '__main__':
